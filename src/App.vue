c70856b3ed9488f684ede1566f5cf14d53127c5a
Gabino Santana <gsantana@dicep.com.ar>
2018-06-22 15:38:35 -0300

    Commit Inicial

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 Lista de Artistas
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" v-bind:value="country.value") {{country.name}}
+    spinner(v-show="loading")
+    ul
+      artist(v-for="artist in artists" v-bind:artist="artist" :key="artist.mbid")
+</template>
+
+<script>
+import Artist from './components/artist.vue'
+import Spinner from './components/spinner.vue'
+import getArtists from './api'
+export default {
+  name: 'app',
+  data () {
+    return {
+      selectedCountry:'argentina',
+      artists:[],
+      countries:[
+        {'name': 'Argentina', value:'argentina'},
+        {'name': 'Colombia', value:'colombia'},
+        {'name': 'España', value:'spain'}
+      ],
+      loading: true
+    }
+  },
+  components:{
+    Artist,
+    Spinner
+  },
+  methods:{
+    refreshArtists(){
+      this.artists =[]
+      this.loading = true
+      const self = this
+      getArtists(this.selectedCountry).then(
+        function (artists){
+          self.loading = false
+          self.artists = artists
+        }
+      )
+    }
+  },
+  mounted: function(){
+    this.refreshArtists()
+  },
+  watch:{
+    selectedCountry:function(){
+      this.refreshArtists()      
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color red
+  margin-top 60px
+
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+  appname: "platzimusic",
+  apiKey: "9915804d9f7f05c5da8dad3101f84f3b",
+  secret: "f4a1b2aacebfdc759aac17a67bdc52b4",
+  registeredTo: "niceforo22"
+};
